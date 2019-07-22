29d50d09fe4b737b0f9b9565a274714cf3bc36e9
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-23 01:02:53 +0200

    more routes changes

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -14,6 +14,19 @@ class Api::V1::MenusController < ApplicationController
     render json: @menu
   end
 
+  def show_current_menu
+    @menu = Menu.find_by(is_current: true, chef_id: params[:id])
+    render json: @menu
+  end
+
+  def set_current_menu
+    old_menu = Menu.find_by(is_current: true, chef_id: current_user.id)
+    old_menu.is_current = false unless old_menu.nil?
+    @menu = Menu.find_by(id: params[:id], chef_id: current_user.id)
+    @menu.is_current = true
+    render json: @menu
+  end
+
   # POST /menus
   def create
     @menu = Menu.new(menu_params)
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1, defaults: { format: :json } do
       
-      
       # customer routes
       get '/chefs/:id/menu', to: 'menus#show_current_menu', as: 'current_menu'
-
       post '/chefs/:id/orders', to: 'orders#create', as: 'new_order'
-
-      get '/customers/:id/orders', to: 'orders#index_customer', as: 'customer_orders'
 
       # shared routes
       get '/orders/:order_id', to: 'orders#show', as: 'order'
@@ -25,19 +21,19 @@ Rails.application.routes.draw do
 
       get '/menus', to: 'menus#index', as: 'menus'
       get '/menus/:id', to: 'menus#show', as: 'menu'
-      get '/items', to: 'items#index', as: 'chef_items'
+      get '/items', to: 'items#index', as: 'items'
       get '/items/:id', to: 'items#show', as: 'item'
 
       patch '/menus/:id', to: 'menus#update', as: 'update_menu'
       patch '/items/:id', to: 'items#update', as: 'update_item'
 
-      post '/chefs/:id/menus', to: 'menus#create', as: 'create_chef_menu'
-      post '/chefs/:id/items', to: 'items#create', as: 'create_chef_item'
+      post '/menus', to: 'menus#create', as: 'create_menu'
+      post '/items', to: 'items#create', as: 'create_item'
 
       delete '/menus/:id', to: 'menus#destroy', as: 'destroy_menu'
       delete '/items/:id', to: 'items#destroy', as: 'destroy_item'
 
-      post '/chefs/:id/menu', to: 'menus#set_current_menu', as: 'set_chef_menu'
+      post '/menus/:id', to: 'menus#set_current_menu', as: 'set_chef_menu'
 
       # get '/chefs/:id/orders', to: 'orders#index', as: 'chef_orders'
     end
